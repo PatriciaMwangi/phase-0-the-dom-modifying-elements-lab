53037fbe3b16c6c1a8a2c88b05afe5e5cf569366
patricia <patricia.mwangi@student.moringaschool.com>
2024-04-06 12:01:11 +0300

    DOM manipulation

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById("main").remove('#main')
+let newHeader=document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id='victory'
+newHeader.textContent="${YOUR-NAME}is the champion"
